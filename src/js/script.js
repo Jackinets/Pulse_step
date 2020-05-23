7f0094e8dd0ccb87c1ecf2a84acb620970dda587
Yevhen N <evgen.nets@gmail.com>
2020-05-23 19:40:06 +0300

    mod .gitignore

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+(function($) {
+    $(function() {
+      
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+      
+    });
+    })(jQuery);
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev')
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next')
+});
